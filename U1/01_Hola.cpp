b5b30f6b71acad3acb896933709dc2b60a8c775e
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-21 10:09:35 -0500

    units

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//Función principal de tipo entero
+int main()
+{
+    //Mediante la instrucción std::cout envia por la pantalla el texto
+    std:: cout<< "Hola mundo/n"; 
+        //Al ser una función debe retornar un valor, en este caso 0
+    return 0;
+
+}
