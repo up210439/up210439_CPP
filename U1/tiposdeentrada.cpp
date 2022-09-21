d97b3a933eb8a01544017659d6c5a00f6713fa75
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-21 10:45:19 -0500

    cout y cin

---
+++ U1/tiposdeentrada.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int numero; float flotante;
+    cout<< "Tecle el número"<< endl;
+    cin>>flotante;
+    cout<<"El número guardado es: " << numero;
+    return 0; 
+}
