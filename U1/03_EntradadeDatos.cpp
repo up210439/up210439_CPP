01d3a5ec93943acb5d39875e8d700c6cef3dfddd
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-21 10:32:59 -0500

    cambios

---
+++ U1/03_EntradadeDatos.cpp
@@ -0,0 +1,35 @@
+//Libreria para manejo de entradas y salida de pantalla
+#include <iostream>
+//Libreria para el uso de printf y scanf
+#include <stdio.h>
+
+//Uso del namespace para evitar el std::
+using namespace std;
+
+//Función principal de tipo entero
+int main()
+{
+    //Mas información en:
+    //https://www.geeksforgeeks.org/c-data-types/
+    int entero;
+    float flotante;
+    double grande;
+    char caracter;
+    
+    cout << "Este programa muestra el ingreso de datos por el usuario. \n";
+    cout << "Ingresa un dato de tipo entero: ";
+    cin >> entero;
+    cout << "El dato ingresado es: " << entero << endl;
+    
+    cout << "=================" << endl;
+
+    //Mas información en:
+    //https://cplusplus.com/reference/cstdio/printf/
+    printf("Usando la función printf y scanf\n");
+    printf("Ingresa un dato de tipo entero: ");
+    scanf("%d", &entero);
+    printf("El dato ingresado es: %i \n", entero);
+
+    //Al ser una función debe retornar un valor, en este caso 0
+    return 0;
+}
