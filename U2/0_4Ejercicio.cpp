7aa7da48fc86c868d5ca6fdde4a54ec8001d26d9
up210439 <up210439@alumnos.upa.edu.mx>
2022-09-29 10:37:42 -0500

    ajustando el ignore

---
+++ U2/0_4Ejercicio.cpp
@@ -0,0 +1,12 @@
+/*  CABECERA DE PROGRAMA
+Date: 29/09/2022
+Author:UP210439
+Description: pizza
+Email: up210439@alumnos.upa.edu.mx
+Last modification: ##
+
+la pizzeria Bella Napoli ofrece pizzas vagetarianas y no vegetarianas a sus
+clientes. Los ingredientes para cada tipo de pizza aparecen a continuacón.
+-ingredientes vegetarianos: pimiento y tofu.
+-ingredientes no vegetarianos: peperoni, jamón y salmón
+escribir un programa que pregunte al usuario */
