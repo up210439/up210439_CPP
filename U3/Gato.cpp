64dfec42a5a2362fd7d37374bd81ce1a31a761d0
UP210659 <UP210659@alumnos.upa.edu.mx>
2022-11-18 09:55:54 -0600

    gato.

--- U3/Gato.cpp
+++ U3/Gato.cpp
@@ -5,12 +5,19 @@ Descripción: juego del gato
 */ 
 
 #include<iostream>
+#include <string.h>
 using namespace std;
 
 //------TABLERO DEL GATO------
 int row,col; 
-char tableroGato[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+string tableroGato[3][3] = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
 void Tablero();
+string HEADER = "\033[95m";
+string CYAN = "\033[96m";
+string VERDE = "\033[92m";
+string Amarrillo = "\033[93m";
+string ENDC = "\033[0m";
+//\033[0;31m"<<variable<<"\033[om/
 
 //------ESTRUCTURA DEL GATO------
 void Tablero() 
@@ -51,7 +58,7 @@ void Tablero()
 //------VARIABLES GLOBALES------
 int turnoJugador = 0; 
 char areaJuego[3][3];
-char valorJugada;
+string valorJugada;
 int turnoJugada;
 bool casillaOcupada;
 void Tablero(int);
@@ -59,11 +66,17 @@ void Tablero(int);
 
 //------FUNCIONES------
 
-int seleccionarJugada(){
+int seleccionarJugada(string j1, string j2){
     int jugada;
     do{
-        cout<< "Dame la jugada: ";
-        cin>>jugada;
+       if(turnoJugador % 2 == 0)//------PARES--------
+       {
+        cout<<"\nTurno de" <<CYAN <<j1 <<ENDC <<", dame la jugada:";
+       }
+       else{
+        cout <<"\nTurno de" <<VERDE <<j2 <<ENDC <<", dame la jugada:";
+       }
+       cin >> jugada;
     }while(jugada < 1 && jugada > 9);
 
     return jugada;
@@ -115,7 +128,7 @@ bool comprobarCasillaOcupada(int jugada){
         row=2;
         col=2;
     }
-    if (tableroGato[row][col] == 'X' || tableroGato[row][col] == 'O')   
+    if (tableroGato[row][col] == "X" || tableroGato[row][col] == "O")   
        return true;
     else
     return false; 
@@ -126,11 +139,11 @@ void colocarJugada(int jugada)
 
     if (turnoJugador % 2 == 0) //------PARES------
     {
-        valorJugada = 'X';
+        valorJugada = CYAN + "X" + ENDC;
     }
     else
     {
-        valorJugada = 'O';
+        valorJugada = VERDE + "O" + ENDC;
     }
     if (jugada == 1)
         tableroGato[0][0] = valorJugada;
@@ -167,17 +180,40 @@ void imprimirAreaJuego(){
 
 
 bool verificacion(){
-    if(tableroGato[0][1] == tableroGato[0][0] && tableroGato[0][2])
+    bool gano = false;
+    if(tableroGato[0][1] == tableroGato[0][0] && tableroGato[0][2] == tableroGato[0][1])
     {
-        else if(tableroGato == tru)
+        gano = true;
     }
-    if(tableroGato[1][1] == tableroGato[1][0] && tableroGato[1][2])
-    if(tableroGato[2][1] == tableroGato[2][0] && tableroGato[2][2])
-    if(tableroGato[1][0] == tableroGato[0][0] && tableroGato[2][0]) 
-    if(tableroGato[1][1] == tableroGato[0][1] && tableroGato[2][1])
-    if(tableroGato[1][2] == tableroGato[0][2] && tableroGato[2][2])
-    if(tableroGato[1][1] == tableroGato[0][0] && tableroGato[2][2])
-    if(tableroGato[1][1] == tableroGato[2][0] && tableroGato[0][2])
+    if(tableroGato[1][1] == tableroGato[1][0] && tableroGato[1][2] == tableroGato[1][1])
+    {
+        gano = true;
+    }
+    if(tableroGato[2][1] == tableroGato[2][0] && tableroGato[2][2] == tableroGato[2][1])
+    {
+        gano = true;
+    }
+    if(tableroGato[1][0] == tableroGato[0][0] && tableroGato[2][0] == tableroGato[1][0]) 
+    {
+        gano = true;
+    }
+    if(tableroGato[1][1] == tableroGato[0][1] && tableroGato[2][1] == tableroGato[1][1])
+    {
+        gano = true;
+    }
+    if(tableroGato[1][2] == tableroGato[0][2] && tableroGato[2][2] == tableroGato[1][2])
+    {
+        gano = true;
+    }
+    if(tableroGato[1][1] == tableroGato[0][0] && tableroGato[2][2] == tableroGato[1][1])
+    {
+        gano = true;
+    }
+    if(tableroGato[1][1] == tableroGato[2][0] && tableroGato[0][2] == tableroGato[1][1])
+    {
+        gano = true;
+    }
+    return gano;
     
     
     
@@ -190,29 +226,41 @@ int main()
     int areaJuego, tablero, jugada, jugador1, modoDeJugada, jugador2;
     bool casillaOcupada = true;
     bool ganador = false;
+    string nombre_j1;
+    string nombre_j2;
     Tablero();
 
     cout << "Dame tu modo de jugada:\n1. VS HUMANO\n2. VS CPU\n";
     cin >> modoDeJugada;
     if (modoDeJugada == 1)
     {
+        cout <<"Quién es jugador 1: ";
+        cin >> nombre_j1;
+        cout << "Quien es juagador 2: ";
+        cin >> nombre_j2;
+        cout << "\n";
 
         do
         {
             do{
-            jugada = seleccionarJugada();
+            jugada = seleccionarJugada(nombre_j1, nombre_j2);
             casillaOcupada = comprobarCasillaOcupada(jugada);
             } while(casillaOcupada == true);
 
             if (casillaOcupada == false)//esta libre
             {
                 colocarJugada(jugada);
-                //ganador = verificacion(ganador);
-
+                ganador = verificacion();
                 system("clear");
                 Tablero();
-                turnoJugador++;//le toca al otro jugador
             }
+            if(ganador)
+            {
+                cout << "\nYa ganaste"
+                     << "\n ";
+                     break;
+            }
+            turnoJugador++;//le toca al otro jugador
 
            
         } while (ganador == false && turnoJugador <= 9);
