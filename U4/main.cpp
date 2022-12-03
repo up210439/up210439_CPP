943c3fde8201aad2089f6974127cae93fb801ebd
Cesar Eduardo Juarez Jasso <112641088+UP210671@users.noreply.github.com>
2022-12-03 14:09:31 -0600

    Update main.cpp

--- U4/main.cpp
+++ U4/main.cpp
@@ -54,6 +54,7 @@ struct pieza{
 	coord peri[3];
 	
 	char D;
+	char d = '*';
 	
 	coord posicion(int n);
 	
@@ -71,83 +72,86 @@ coord pieza::posicion(int n)
 }
 
 
-coord peris[4][3] = {{{2,0},{4,0},{6,0}},
+coord peris[5][3] = {{{2,0},{4,0},{6,0}},
 					{{2,0},{4,0},{  }},
 					{{2,0},{4,0},{  }},
+					{{   },{   },{  }},
 					{{   },{   },{  }},};
 
 char barcos[4] = {'P', 'S', 'D', 'F'};
-class enemigo{
-	
-    private:
 
-        public:
-
-        	void pintarmapa();
-        	void pintar(int, int);
-        	void borrar(int, int);
-        	void mover(int &, int &);
-};
-
-void enemigo::pintarmapa()
-{
-    for(int i=0;i<22;i++)
-    {
-        cout<<endl;
-        for(int j=0;j<22;j++)
-        {
-          cout<<ma[i][j];  
-        }
-    }
-}
-
-void enemigo::pintar(int x, int y)
-{
-    gotoxy(x,y);cout<<"O";
-}
-
-void enemigo::borrar(int x, int y)
-{
-    gotoxy(x,y);cout<<" ";
-}
-
-void enemigo::mover(int &x, int &y)
-{
-    if (kbhit())
-    {
-        enemigo::borrar(x,y);
-        char tecla = getch();
-        if(tecla == 'a' && x>2) x=x-2;
-        if(tecla == 'd' && x<20) x=x+2;
-        if(tecla == 'w' && y>3) y=y-2;
-        if(tecla == 's' && y<21) y=y+2;
-        enemigo::pintar(x,y);
-    }
-
-}
+char ju[22][22];
+char en[22][22];
 
 class jugador{
 	
 	public:
 		
-		void pintar(pieza &);
+		void pasarmapa();
+		void dibujarmapaju();
+		void pintar(pieza &, int);
+		void borrar(pieza &);
 		void rotar(pieza &);
 		coord rotar(coord &);
 		void seleccinar(pieza &, int);
+		void mover(pieza &, int &);
+		bool colision(pieza &);
 
 };
 
-void jugador::pintar(pieza &P)
+void jugador::pasarmapa()
+{
+	for(int i=0;i<22;i++)
+    {
+        cout<<endl;
+        for(int j=0;j<22;j++)
+        {
+          ju[i][j] = ma[i][j];  
+          en[i][j] = ma[i][j];
+        }
+    }
+}
+
+void jugador::dibujarmapaju()
+{
+	for(int i=0;i<22;i++)
+    {
+        cout<<endl;
+        for(int j=0;j<22;j++)
+        {
+          cout<<ju[i][j];  
+        }
+    }
+}
+
+void jugador::pintar(pieza &P, int r)
+{
+	if(r == 1)
+	{
+		for(int i=0; i<4; i++)
+		{
+		coord c = P.posicion(i);
+		ju[c.y][c.x] = P.d;
+		}
+	}
+	else
+	{
+		for(int i=0; i<4; i++)
+		{
+		coord c = P.posicion(i);
+		ju[c.y][c.x] = P.D;
+		}
+	}
+}
+
+void jugador::borrar(pieza &P)
 {	
 	for(int i=0; i<4; i++)
 	{
 		coord c = P.posicion(i);
-		ma[c.x][c.y]  = P.D;	
+		ju[c.y][c.x]  =  ' ';	
 	}
-	
-
 }
-
 
 coord jugador::rotar(coord &c)
 {
@@ -175,33 +179,155 @@ void jugador::seleccinar(pieza &P, int r)
 	}
 }
 
+void jugador::mover(pieza &P, int &r)
+{
+	if (kbhit())
+    {
+        pieza copia = P;
+		char tecla = getch();
+        if(tecla == 'a') P.ori.x -=2;
+        if(tecla == 'd') P.ori.x +=2;
+        if(tecla == 'w') P.ori.y -=2;
+        if(tecla == 's') P.ori.y +=2;
+        
+        if(tecla =='c')
+        {
+        	jugador::rotar(P);
+		}
+        
+        if(jugador::colision(P))
+        {
+        	P = copia;
+		}
+		
+        jugador::borrar(copia);
+        jugador::pintar(P,1);
+    
+		if(tecla == 'x')
+		{
+			jugador::pintar(P,2);
+			r++;
+			jugador::seleccinar(P,r);
+		}
+	}
+}
+
+bool jugador::colision(pieza &P)
+{
+	for(int i=0; i<4; i++)
+	{
+		coord c = P.posicion(i);
+		
+		if(c.x < 2 || c.x > 20)return true;
+		
+		if(c.y < 2 || c.y > 20)return true;	
+	
+		if(ju[c.y][c.x] == 'P' || ju[c.y][c.x] == 'S' || ju[c.y][c.x] == 'D' || ju[c.y][c.x] == 'F')return true;
+	}
+	
+	return false;
+}
+
+class enemigo
+{
+	public:
+		void dibujarmapaen();
+		void pintar(pieza &, int);
+		void borrar(pieza &);
+		void seleccinar(pieza &, int);
+		void mover(pieza &, int &);
+		bool colision(pieza &);
+		
+};
+
+void enemigo::dibujarmapaen()
+{
+	for(int i=0;i<22;i++)
+    {
+        cout<<endl;
+        for(int j=0;j<22;j++)
+        {
+          cout<<en[i][j];  
+        }
+    }
+}
+
+void enemigo::pintar(pieza &P,int r)
+{
+	if(r == 1)
+	{
+		for(int i=0; i<4; i++)
+		{
+		coord c = P.posicion(i);
+		en[c.y][c.x] = P.d;
+		}
+	}
+	else
+	{
+		for(int i=0; i<4; i++)
+		{
+		coord c = P.posicion(i);
+		en[c.y][c.x] = P.D;
+		}
+	}
+}
+
+void enemigo::borrar(pieza &P)
+{
+	for(int i=0; i<4; i++)
+	{
+		coord c = P.posicion(i);
+		en[c.y][c.x]  =  ' ';	
+	}
+}
+
+void enemigo::mover(pieza &P, int &r)
+{
+	if (kbhit())
+    {
+        pieza copia = P;
+		char tecla = getch();
+        if(tecla == 'a') P.ori.x -=2;
+        if(tecla == 'd') P.ori.x +=2;
+        if(tecla == 'w') P.ori.y -=2;
+        if(tecla == 's') P.ori.y +=2;
+        
+        
+        enemigo::borrar(copia);
+        enemigo::pintar(P,1);
+    
+	}
+}
 int main ()
 {
 	
-    enemigo a;
-    jugador b;
-    
+    jugador a;
+    enemigo b;
     
     pieza S;
-    int r = 2;
+    int r = 0;
+    int t=0;
     
+    a.pasarmapa();
+    a.seleccinar(S,r);
     
-    b.seleccinar(S,r);
-    
-	//a.pintarmapa();
+	while(true)
+	{
+		gotoxy(0,25); a.dibujarmapaju();
+		gotoxy(0,0); b.dibujarmapaen();
+		
+		if(t == 0)
+		{
+			a.mover(S,r);
+			if(r == 4) t=1;
+		}
+		if(t == 1)
+		{
+			b.mover(S,r);
+		}
+	}
 	
-	b.rotar(S);
-	b.pintar(S);
-    gotoxy(0,25);
-    a.pintarmapa();
-    
-    
-	//b.rotar(c);
-	
-	//b.pintar(c);
-	
-
-    
+ 
  
  	return 0;   
     
