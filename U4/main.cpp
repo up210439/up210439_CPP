9cc0c2aa1e390c034035df0a810cc0c278eded64
Cesar Eduardo Juarez Jasso <112641088+UP210671@users.noreply.github.com>
2022-12-05 09:57:34 -0600

    Update main.cpp

--- U4/main.cpp
+++ U4/main.cpp
@@ -6,8 +6,6 @@ Evenly Liliana Delgado Rivera
 #include<iostream>
 #include<windows.h>
 #include<conio.h>
-#include<stdlib.h>
-#include<time.h>
 
 using namespace std;
 
@@ -84,14 +82,9 @@ char barcos[4] = {'P', 'S', 'D', 'F'};
 
 char ju[22][22];
 char en[22][22];
-char en2[22][22];
 
 class jugador{
 	
-	private:
-		int turno = 0;
-		int turno1;
-		
 	public:
 		
 		void pasarmapa();
@@ -102,13 +95,8 @@ class jugador{
 		coord rotar(coord &);
 		void seleccinar(pieza &, int);
 		void mover(pieza &, int &);
-		bool colision(pieza &, int);
-		int obtenerxy();
-		void seleccionamapa(pieza &, int, int);
-		void disparo(pieza &, int);
-		int verificar(pieza &);
-		void IAgeneral();
-		void cambioturno(pieza &, int &, int);
+		bool colision(pieza &);
+
 };
 
 void jugador::pasarmapa()
@@ -120,7 +108,6 @@ void jugador::pasarmapa()
         {
           ju[i][j] = ma[i][j];  
           en[i][j] = ma[i][j];
-          en2[i][j] = ma[i][j];
         }
     }
 }
@@ -139,13 +126,22 @@ void jugador::dibujarmapaju()
 
 void jugador::pintar(pieza &P, int r)
 {
+	if(r == 1)
+	{
 		for(int i=0; i<4; i++)
 		{
 		coord c = P.posicion(i);
-		if(r == 1)ju[c.y][c.x] = P.d;
-		else ju[c.y][c.x] = P.D;
+		ju[c.y][c.x] = P.d;
 		}
-
+	}
+	else
+	{
+		for(int i=0; i<4; i++)
+		{
+		coord c = P.posicion(i);
+		ju[c.y][c.x] = P.D;
+		}
+	}
 }
 
 void jugador::borrar(pieza &P)
@@ -174,8 +170,8 @@ void jugador::rotar(pieza &P)
 
 void jugador::seleccinar(pieza &P, int r)
 {
-	P.ori.x = jugador::obtenerxy();
-	P.ori.y = jugador::obtenerxy();
+	P.ori.x = 2;
+	P.ori.y = 2;
 	P.D = barcos[r];
 	for(int i = 0; i < 3; i++)
 	{
@@ -199,7 +195,7 @@ void jugador::mover(pieza &P, int &r)
         	jugador::rotar(P);
 		}
         
-        if(jugador::colision(P,1))
+        if(jugador::colision(P))
         {
         	P = copia;
 		}
@@ -208,15 +204,15 @@ void jugador::mover(pieza &P, int &r)
         jugador::pintar(P,1);
     
 		if(tecla == 'x')
-		{ 
+		{
 			jugador::pintar(P,2);
 			r++;
-			jugador::seleccionamapa(P,r,1);
+			jugador::seleccinar(P,r);
 		}
 	}
 }
 
-bool jugador::colision(pieza &P, int q)
+bool jugador::colision(pieza &P)
 {
 	for(int i=0; i<4; i++)
 	{
@@ -225,102 +221,25 @@ bool jugador::colision(pieza &P, int q)
 		if(c.x < 2 || c.x > 20)return true;
 		
 		if(c.y < 2 || c.y > 20)return true;	
-		
-		if(q == 1)
-		{
-			if(ju[c.y][c.x] == 'P' || ju[c.y][c.x] == 'S' || ju[c.y][c.x] == 'D' || ju[c.y][c.x] == 'F')return true;
-		}
-		else
-		{
-			if(ju[c.y][c.x] == 'X' || ju[c.y][c.x] == 'O') return true;	
-		}
+	
+		if(ju[c.y][c.x] == 'P' || ju[c.y][c.x] == 'S' || ju[c.y][c.x] == 'D' || ju[c.y][c.x] == 'F')return true;
 	}
 	
 	return false;
 }
 
-int jugador::obtenerxy()
-{
-	while(true)
-	{
-		int x = rand()%19 + 2;
-		if(x%2 == 0) return x;
-	}	
-}
-
-void jugador::seleccionamapa(pieza &P, int r, int q)
-{
-	while(true)
-	{
-	
-	jugador::seleccinar(P,r);
-	if(jugador::colision(P,q)) break;
-	}
-}
-
-void jugador::disparo(pieza &P, int q)
-{
-	for(int i = 0; i < 4; i ++)
-	{
-		coord c = P.posicion(i);
-		
-		if(q == 1)
-		{
-			if(en2[c.y][c.x] == 'P' || en2[c.y][c.x] == 'S' || en2[c.y][c.x] == 'D' en2[c.y][c.x] == 'F') en[c.y[c.x] = 'O';
-			if(en2[c.y][c.x] == ' ') en[c.y][c.x] = 'X';
-		}
-		else 
-		{
-			if(ju[c.y][c.x] == 'P' || ju[c.y][c.x] == 'S' || ju[c.y][c.x] == 'D' ju[c.y][c.x] == 'F') 
-			{
-				if(ju[c.y[c.x] = 'P') P.D = 'P';
-				if(ju[c.y[c.x] = 'P') P.D = 'S';
-				if(ju[c.y[c.x] = 'P') P.D = 'D';
-				ju[c.y[c.x] = 'O';
-				turno1 = 1; 
-			}
-			if(ju[c.y][c.x] == ' ') ju[c.y][c.x] = 'X';	
-		}
-	}
-}
-
-int jugador::verificar(pieza &P)
-{
-	for(int i = 2; i < 21; i ++)
-    {
-        for(int j = 2; j < 21; j ++)
-        {
-          if(ju([i][j] == P.D)
-          {
-          	P.ori.x = j;
-          	P.ori.y = i;
-          	return 1;
-		  }
-        }
-    }
-    return 2;
-}
-
-void jugador::cambioturno(pieza &P, int &t, int r)
-{
-	jugador::seleccionamapa(P,r.2);
-	jugador::disparo(P,2);
-	t = 1; 
-	
-}
-class enemigo : public jugador 
+class enemigo
 {
 	public:
-		
 		void dibujarmapaen();
 		void pintar(pieza &, int);
 		void borrar(pieza &);
-		void mover(pieza &, int &, int &);
-		bool colision(pieza &, int);
-		void seleccionamapa(pieza &, int);
-		void inicializarbarcos(pieza &, int);
-		int obtenerrot();
+		void seleccinar(pieza &, int);
+		void mover(pieza &, int &);
+		bool colision(pieza &);
+		
 };
+
 
 void enemigo::dibujarmapaen()
 {
@@ -336,11 +255,21 @@ void enemigo::dibujarmapaen()
 
 void enemigo::pintar(pieza &P,int r)
 {
-	for(int i=0; i<4; i++)
+	if(r == 1)
 	{
+		for(int i=0; i<4; i++)
+		{
 		coord c = P.posicion(i);
-		if(r == 1)en[c.y][c.x] = P.d;
-		else en2[c.y][c.x] = P.D;
+		en[c.y][c.x] = P.d;
+		}
+	}
+	else
+	{
+		for(int i=0; i<4; i++)
+		{
+		coord c = P.posicion(i);
+		en[c.y][c.x] = P.D;
+		}
 	}
 }
 
@@ -353,7 +282,7 @@ void enemigo::borrar(pieza &P)
 	}
 }
 
-void enemigo::mover(pieza &P, int &r, int &t)
+void enemigo::mover(pieza &P, int &r)
 {
 	if (kbhit())
     {
@@ -364,95 +293,24 @@ void enemigo::mover(pieza &P, int &r, int &t)
         if(tecla == 'w') P.ori.y -=2;
         if(tecla == 's') P.ori.y +=2;
         
-        if(enemigo::colision(P,2))
-        {
-        	P=copia;
-		}
         
         enemigo::borrar(copia);
         enemigo::pintar(P,1);
-        
-        if(tecla == 'z')
-        {
-        	enemigo::disparo(P,1);
-			enemigo::seleccionamapa(P,r);
-			t = 2;
-			copia = P;		
-		}
-    
-	}	
-}
-
-bool enemigo::colision(pieza &P, int q)
-{
-	for(int i=0; i<4; i++)
-	{
-		coord c = P.posicion(i);
-		
-		if(c.x < 2 || c.x > 20)return true;
-		
-		if(c.y < 2 || c.y > 20)return true;	
-		
-		if(q == 1)
-		{
-			if(en2[c.y][c.x] == 'P' || en2[c.y][c.x] == 'S' || en2[c.y][c.x] == 'D' || en2[c.y][c.x] == 'F')return true;
-		}
-		else
-		{	
-			if(en[c.y][c.x] == 'X' || en[c.y][c.x] == 'O') return true;
-		}
-	}
-	
-	return false;
-}
-
-void enemigo::seleccionamapa(pieza &P, int r)
-{
-	while(true)
-	{
-	enemigo::seleccinar(P,r);
-	if(enemigo::colision(P,2)) break;
 	}
 }
-
-void enemigo::inicializarbarcos(pieza &P)
-{
-	for(int i = 0; i < 5; i ++)
-	{
-		while(true)
-		{
-			enemigo::seleccinar(P,i);
-			if(enemigo::obtenerrot() == 0) enemigo::rotar(P);
-			if(enemigo::colision(P,1)) 
-			{
-				enemigo::pintar(P,2);
-				break;
-			}	 
-		}
-	}
-}
-
-int enemigo::obtenerrot()
-{
-	int x = rand()%2;
-	return x; 
-}
-
 int main ()
+
 {
-	//rand()
-	srand(time(NULL));
+	
     jugador a;
     enemigo b;
     
-    pieza S, S1;
+    pieza S;
     int r = 0;
     int t=0;
     
     a.pasarmapa();
     a.seleccinar(S,r);
-    
-    b.inicializarbarcos(S1);
     
 	while(true)
 	{
@@ -466,7 +324,7 @@ int main ()
 		}
 		if(t == 1)
 		{
-			b.mover(S1,r,t);
+			b.mover(S,r);
 		}
 	}
 	
